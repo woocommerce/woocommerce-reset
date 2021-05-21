2a781d925ba1a20e23dca5a0c1cd0bb82a754f99
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-05-21 15:17:25 +0800

    Add project files

---
+++ woocommerce-reset.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WooCommerce Reset
+ * Plugin URI:      https://github.com/woocommerce/woocommerce-reset
+ * Description:     A developer tool for WooCommerce provding REST API endpoints to reset WooCommerce data.
+ * Author:          WooCommerce
+ * Author URI:      https://woocommerce.com/
+ * Text Domain:     woocommerce-reset
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WooCommerce_Reset
+ */
+
+namespace Automattic\WooCommerce\Reset;
