cbc9bb15f16f81834058e283b912b1acfdfe34d9
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-05-25 11:18:59 +0800

    Fix REST test not asserting options deleted

--- tests/test-rest.php
+++ tests/test-rest.php
@@ -8,7 +8,7 @@
 namespace Automattic\WooCommerce\Reset;
 
 /**
- * REST test case.
+ * Integration test for reset endpoint.
  */
 class RestTest extends \WP_UnitTestCase {
 
@@ -24,9 +24,9 @@ class RestTest extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * Test the reset endpoint.
+	 * Test the reset endpoint deletes all woocommerce options.
 	 */
-	public function test_reset_endpoint() {
+	public function test_reset_endpoint_deletes_woocommerce_options() {
 		global $wp_rest_server;
 
 		$routes = $wp_rest_server->get_routes();
@@ -38,5 +38,16 @@ class RestTest extends \WP_UnitTestCase {
 		$response = $wp_rest_server->dispatch( $request );
 
 		$this->assertEquals( 200, $response->get_status() );
+
+		$this->assertFalse( get_option( 'woocommerce_allow_tracking' ) );
+		$this->assertFalse( get_option( 'woocommerce_onboarding_profile' ) );
+		$this->assertFalse( get_option( 'woocommerce_task_list_welcome_modal_dismissed' ) );
+		$this->assertFalse( get_option( 'woocommerce_task_list_tracked_completed_tasks' ) );
+		$this->assertFalse( get_option( 'woocommerce_ces_tracks_queue' ) );
+		$this->assertFalse( get_option( 'woocommerce_clear_ces_tracks_queue_for_page' ) );
+		$this->assertFalse( get_option( 'wc_remote_inbox_notifications_stored_state' ) );
+		$this->assertFalse( get_option( 'wc_remote_inbox_notifications_specs' ) );
+		$this->assertFalse( get_option( 'wc_remote_inbox_notifications_wca_updated' ) );
+		$this->assertFalse( get_option( 'woocommerce_admin_install_timestamp' ) );
 	}
 }
